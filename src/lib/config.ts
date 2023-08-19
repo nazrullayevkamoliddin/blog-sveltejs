9e840ea18fbf730f41ed6d7f21244548ce19aedd
nazrullayevkamoliddin <kamolxoja.n@gmail.com>
2023-08-19 19:57:40 +0500

    first commit

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment";
+
+export const title = 'CyberXoja';
+export const description = 'SvelteKit blog for Red Hackers';
+export const url = dev ? 'http://localhost:5173/' : 'http://blog-sveltejs.vercel.app';

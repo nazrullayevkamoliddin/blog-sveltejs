3ac1bb76ba0154ca0f93d01eec17ad3bdacc2e75
nazrullayevkamoliddin <kamolxoja.n@gmail.com>
2023-08-20 06:55:06 +0500

    syntax highlighing

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
-
-import { mdsvex } from 'mdsvex';
+import shiki from 'shiki'
+import { escapeSvelte, mdsvex } from 'mdsvex';
 
 const mdsvexOptions = {
-	extensions: ['.md']
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await shiki.getHighlighter({ theme: 'poimandres' })
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+			return `{@html \`${html}\` }`
+		}
+	}
 }
 
 /** @type {import('@sveltejs/kit').Config} */
